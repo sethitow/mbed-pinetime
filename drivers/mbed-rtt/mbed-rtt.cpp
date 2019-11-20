bc216104f1907fd13293a088fa627d812525a397
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-11-20 14:12:26 -0800

    RTT: Don't block if FIFO is full

--- drivers/mbed-rtt/mbed-rtt.cpp
+++ drivers/mbed-rtt/mbed-rtt.cpp
@@ -7,7 +7,7 @@
 SeggerRTT::SeggerRTT(void)
 {
     /* Initialize ITM using internal init function. */
-	SEGGER_RTT_ConfigUpBuffer(MBED_SEGGER_RTT_TERMINAL_INDEX, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+	SEGGER_RTT_ConfigUpBuffer(MBED_SEGGER_RTT_TERMINAL_INDEX, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_SKIP);
 }
 
 ssize_t SeggerRTT::write(const void *buffer, size_t size)
