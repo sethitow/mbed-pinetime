3f5f6b135ccc21351329b24183d9f3df51174a0d
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-11-20 16:27:27 -0800

    BMA4 generic driver -- doesn't work without binary blob

--- apps/periph_test/source/main.cpp
+++ apps/periph_test/source/main.cpp
@@ -6,8 +6,9 @@
 #include "mbed-rtt.h"
 #include "rtos/Mutex.h"
 #include "rtos/ThisThread.h"
-
-#include "bma400.h"
+extern "C" {
+#include "bma4.h"
+}
 
 #include "mbed-trace/mbed_trace.h"
 #define TRACE_GROUP "main"
@@ -21,13 +22,12 @@ FileHandle *mbed_override_console(int fd)
 }
 } // namespace mbed
 
-void set_interface(enum bma400_intf intf, struct bma400_dev *dev);
 void delay_ms(uint32_t period);
-int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
+uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
                      uint16_t length);
-int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
+uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
                     uint16_t length);
-void print_rslt(int8_t rslt);
+void print_rslt(uint16_t rslt);
 
 mbed::DigitalIn main_button(p15, PullNone);
 mbed::DigitalOut vibrator(PIN_VIBRATOR_OUT);
@@ -63,62 +63,92 @@ int main()
 
     i2c.frequency(100000);
 
-    struct bma400_dev bma;
-    struct bma400_sensor_conf conf;
-    struct bma400_sensor_data data;
+    struct bma4_dev bma;
+    struct bma4_accel_config conf;
+    struct bma4_accel data;
     int8_t rslt;
 
-    set_interface(BMA400_I2C_INTF, &bma);
+    // set_interface(BMA400_I2C_INTF, &bma);
 
-    rslt = bma400_init(&bma);
-    print_rslt(rslt);
+    // rslt = bma4_init(&bma);
+    // print_rslt(rslt);
 
-    rslt = bma400_soft_reset(&bma);
-    print_rslt(rslt);
+    // rslt = bma4_soft_reset(&bma);
+    // print_rslt(rslt);
 
-    /* Select the type of configuration to be modified */
-    conf.type = BMA400_ACCEL;
+	/* Modify the parameters */
+	bma.dev_addr = BMA4_I2C_ADDR_PRIMARY;
+	bma.interface = BMA4_I2C_INTERFACE;
+	bma.bus_read = i2c_reg_read;
+	bma.bus_write = i2c_reg_write;
+	bma.delay = delay_ms;
+	bma.read_write_len = 8;
 
-    /* Get the accelerometer configurations which are set in the sensor */
-    rslt = bma400_get_sensor_conf(&conf, 1, &bma);
-    print_rslt(rslt);
+	// From Init Function
+	bma.variant = BMA42X_VARIANT;	
+	bma.resolution = 12;
 
-    /* Modify the desired configurations as per macros
-     * available in bma400_defs.h file */
-    conf.param.accel.odr = BMA400_ODR_100HZ;
-    conf.param.accel.range = BMA400_2G_RANGE;
-    conf.param.accel.data_src = BMA400_DATA_SRC_ACCEL_FILT_1;
+	tr_info("Accel Init");
+	rslt = bma4_init(&bma);
+	print_rslt(rslt);
 
-    /* Set the desired configurations to the sensor */
-    rslt = bma400_set_sensor_conf(&conf, 1, &bma);
-    print_rslt(rslt);
+	tr_info("Chip ID: %hu", bma.chip_id);
 
-    rslt = bma400_set_power_mode(BMA400_LOW_POWER_MODE, &bma);
-    print_rslt(rslt);
+	conf.odr = BMA4_OUTPUT_DATA_RATE_100HZ;	
+	conf.range = BMA4_ACCEL_RANGE_2G;
+	conf.bandwidth = BMA4_ACCEL_NORMAL_AVG4;
+	conf.perf_mode = BMA4_CONTINUOUS_MODE;
 
-    printf("I2C Scan...\r\n");
-    for (int i = 0; i < 128; i++)
-    {
-        i2c.start();
-        int ret = i2c.write(i << 1);
-        if (ret == 1)
-        {
-            printf("0x%x ACK \r\n", i); // Send command string
-        }
-        else
-        {
-        	printf("0x%x NO ACK, returned %i \r\n", i, ret); // Send command string
-        }
-        i2c.stop();
-    }
-    printf("I2C Scan End \r\n");
+	tr_info("Accel Set Config");
+	rslt = bma4_set_accel_config(&conf, &bma);
+	print_rslt(rslt);
+
+
+
+
+    // /* Select the type of configuration to be modified */
+    // conf.type = BMA400_ACCEL;
+
+    // /* Get the accelerometer configurations which are set in the sensor */
+    // rslt = bma400_get_sensor_conf(&conf, 1, &bma);
+    // print_rslt(rslt);
+
+    // /* Modify the desired configurations as per macros
+    //  * available in bma400_defs.h file */
+    // conf.param.accel.odr = BMA400_ODR_100HZ;
+    // conf.param.accel.range = BMA400_2G_RANGE;
+    // conf.param.accel.data_src = BMA400_DATA_SRC_ACCEL_FILT_1;
+
+    // /* Set the desired configurations to the sensor */
+    // rslt = bma400_set_sensor_conf(&conf, 1, &bma);
+    // print_rslt(rslt);
+
+    // rslt = bma400_set_power_mode(BMA400_LOW_POWER_MODE, &bma);
+    // print_rslt(rslt);
+
+    // printf("I2C Scan...\r\n");
+    // for (int i = 0; i < 128; i++)
+    // {
+    //     i2c.start();
+    //     int ret = i2c.write(i << 1);
+    //     if (ret == 1)
+    //     {
+    //         printf("0x%x ACK \r\n", i); // Send command string
+    //     }
+    //     else
+    //     {
+    //     	printf("0x%x NO ACK, returned %i \r\n", i, ret); // Send command string
+    //     }
+    //     i2c.stop();
+    // }
+    // printf("I2C Scan End \r\n");
 
     while (true)
     {
         float battery_voltage_v = (battery_voltage.read() * 3.3) * 2;
         tr_info("Main Button: %d. Batt Volt: %f.", main_button.read(), battery_voltage_v);
 
-        rslt = bma400_get_accel_data(BMA400_DATA_SENSOR_TIME, &data, &bma);
+        rslt = bma4_read_accel_xyz(&data, &bma);
         print_rslt(rslt);
 
         tr_info("Accel Data: %i, %i, %i.", data.x, data.y, data.z);
@@ -175,26 +205,26 @@ int main()
     }
 }
 
-void print_rslt(int8_t rslt)
+void print_rslt(uint16_t rslt)
 {
     switch (rslt)
     {
-    case BMA400_OK:
+    case BMA4_OK:
         tr_info("Success.");
         break;
-    case BMA400_E_NULL_PTR:
+    case BMA4_E_NULL_PTR:
         tr_err("Error [%d] : Null pointer", rslt);
         break;
-    case BMA400_E_COM_FAIL:
-        tr_err("Error [%d] : Communication failure", rslt);
+    // case BMA400_E_COM_FAIL:
+    //     tr_err("Error [%d] : Communication failure", rslt);
+    //     break;
+    // case BMA400_E_DEV_NOT_FOUND:
+    //     tr_err("Error [%d] : Device not found", rslt);
+    //     break;
+    case BMA4_E_INVALID_SENSOR:
+        tr_err("Error [%d] : Invalid sensor", rslt);
         break;
-    case BMA400_E_DEV_NOT_FOUND:
-        tr_err("Error [%d] : Device not found", rslt);
-        break;
-    case BMA400_E_INVALID_CONFIG:
-        tr_err("Error [%d] : Invalid configuration", rslt);
-        break;
-    case BMA400_W_SELF_TEST_FAIL:
+    case BMA4_E_SELF_TEST_FAIL:
         tr_err("Warning [%d] : Self test failed", rslt);
         break;
     default:
@@ -203,7 +233,7 @@ void print_rslt(int8_t rslt)
     }
 }
 
-int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
+uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
                      uint16_t length)
 {
 
@@ -228,7 +258,7 @@ int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
     return -1;
 }
 
-int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
+uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
                     uint16_t length)
 {
 
@@ -256,19 +286,19 @@ int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
 
 void delay_ms(uint32_t period) { rtos::ThisThread::sleep_for(period); }
 
-void set_interface(enum bma400_intf intf, struct bma400_dev *dev)
-{
-    switch (intf)
-    {
-    case BMA400_I2C_INTF:
-        dev->intf_ptr = NULL; /* To attach your interface device reference */
-        dev->delay_ms = delay_ms;
-        dev->dev_id = BMA400_I2C_ADDRESS_SDO_HIGH;
-        dev->read = i2c_reg_read;
-        dev->write = i2c_reg_write;
-        dev->intf = BMA400_I2C_INTF;
-        break;
-    default:
-        tr_err("Interface not supported.");
-    }
-}
+// void set_interface(enum bma400_intf intf, struct bma400_dev *dev)
+// {
+//     switch (intf)
+//     {
+//     case BMA400_I2C_INTF:
+//         dev->intf_ptr = NULL; /* To attach your interface device reference */
+//         dev->delay_ms = delay_ms;
+//         dev->dev_id = BMA400_I2C_ADDRESS_SDO_HIGH;
+//         dev->read = i2c_reg_read;
+//         dev->write = i2c_reg_write;
+//         dev->intf = BMA400_I2C_INTF;
+//         break;
+//     default:
+//         tr_err("Interface not supported.");
+//     }
+// }
