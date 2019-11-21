e640403270575d11603663200c09e2d18e5d178c
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-11-20 19:24:08 -0800

    Accel Raw Read works!

--- apps/periph_test/source/main.cpp
+++ apps/periph_test/source/main.cpp
@@ -6,9 +6,10 @@
 #include "mbed-rtt.h"
 #include "rtos/Mutex.h"
 #include "rtos/ThisThread.h"
-extern "C" {
-#include "bma4.h"
-}
+
+#include "bma421_hack.h"
+
+#include <bitset>
 
 #include "mbed-trace/mbed_trace.h"
 #define TRACE_GROUP "main"
@@ -23,10 +24,8 @@ FileHandle *mbed_override_console(int fd)
 } // namespace mbed
 
 void delay_ms(uint32_t period);
-uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
-                     uint16_t length);
-uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
-                    uint16_t length);
+uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 void print_rslt(uint16_t rslt);
 
 mbed::DigitalIn main_button(p15, PullNone);
@@ -57,6 +56,11 @@ int main()
     lcd_cs = true;   // Disable LCD SPI communications
     vibrator = true; // Turns the vibrator off
 
+    // Turn the backlight off
+    backlight_high = true;
+    backlight_mid = true;
+    backlight_low = true;
+
     mbed::BlockDevice *bd = mbed::BlockDevice::get_default_instance();
     tr_info("BD init returned %i", bd->init());
     tr_info("BD Size %lld.", bd->size());
@@ -68,63 +72,38 @@ int main()
     struct bma4_accel data;
     int8_t rslt;
 
-    // set_interface(BMA400_I2C_INTF, &bma);
+    /* Modify the parameters */
+    bma.dev_addr = BMA4_I2C_ADDR_PRIMARY;
+    bma.interface = BMA4_I2C_INTERFACE;
+    bma.bus_read = i2c_reg_read;
+    bma.bus_write = i2c_reg_write;
+    bma.delay = delay_ms;
+    bma.read_write_len = 8;
 
-    // rslt = bma4_init(&bma);
+    // From Init Function
+    bma.variant = BMA42X_VARIANT;
+    bma.resolution = 12;
+
+    tr_info("Accel Init");
+    rslt = bma421_init(&bma);
+    print_rslt(rslt);
+
+    // tr_info("Write Feature Config Blob File");
+    // rslt = bma421_write_config_file(&bma);
     // print_rslt(rslt);
 
-    // rslt = bma4_soft_reset(&bma);
-    // print_rslt(rslt);
+    tr_info("Accel Enable");
+    rslt = bma4_set_accel_enable(BMA4_ENABLE, &bma);
+    print_rslt(rslt);
 
-	/* Modify the parameters */
-	bma.dev_addr = BMA4_I2C_ADDR_PRIMARY;
-	bma.interface = BMA4_I2C_INTERFACE;
-	bma.bus_read = i2c_reg_read;
-	bma.bus_write = i2c_reg_write;
-	bma.delay = delay_ms;
-	bma.read_write_len = 8;
+    conf.odr = BMA4_OUTPUT_DATA_RATE_100HZ;
+    conf.range = BMA4_ACCEL_RANGE_2G;
+    conf.bandwidth = BMA4_ACCEL_NORMAL_AVG4;
+    conf.perf_mode = BMA4_CONTINUOUS_MODE;
 
-	// From Init Function
-	bma.variant = BMA42X_VARIANT;	
-	bma.resolution = 12;
-
-	tr_info("Accel Init");
-	rslt = bma4_init(&bma);
-	print_rslt(rslt);
-
-	tr_info("Chip ID: %hu", bma.chip_id);
-
-	conf.odr = BMA4_OUTPUT_DATA_RATE_100HZ;	
-	conf.range = BMA4_ACCEL_RANGE_2G;
-	conf.bandwidth = BMA4_ACCEL_NORMAL_AVG4;
-	conf.perf_mode = BMA4_CONTINUOUS_MODE;
-
-	tr_info("Accel Set Config");
-	rslt = bma4_set_accel_config(&conf, &bma);
-	print_rslt(rslt);
-
-
-
-
-    // /* Select the type of configuration to be modified */
-    // conf.type = BMA400_ACCEL;
-
-    // /* Get the accelerometer configurations which are set in the sensor */
-    // rslt = bma400_get_sensor_conf(&conf, 1, &bma);
-    // print_rslt(rslt);
-
-    // /* Modify the desired configurations as per macros
-    //  * available in bma400_defs.h file */
-    // conf.param.accel.odr = BMA400_ODR_100HZ;
-    // conf.param.accel.range = BMA400_2G_RANGE;
-    // conf.param.accel.data_src = BMA400_DATA_SRC_ACCEL_FILT_1;
-
-    // /* Set the desired configurations to the sensor */
-    // rslt = bma400_set_sensor_conf(&conf, 1, &bma);
-    // print_rslt(rslt);
-
-    // rslt = bma400_set_power_mode(BMA400_LOW_POWER_MODE, &bma);
-    // print_rslt(rslt);
+    tr_info("Accel Set Config");
+    rslt = bma4_set_accel_config(&conf, &bma);
+    print_rslt(rslt);
 
     // printf("I2C Scan...\r\n");
     // for (int i = 0; i < 128; i++)
@@ -146,63 +125,26 @@ int main()
     while (true)
     {
         float battery_voltage_v = (battery_voltage.read() * 3.3) * 2;
-        tr_info("Main Button: %d. Batt Volt: %f.", main_button.read(), battery_voltage_v);
-
         rslt = bma4_read_accel_xyz(&data, &bma);
-        print_rslt(rslt);
+        if (rslt)
+        {
+            print_rslt(rslt);
+        }
 
-        tr_info("Accel Data: %i, %i, %i.", data.x, data.y, data.z);
+        tr_info("Main Button: %d. Batt Volt: %f. Accel Data: %i, %i, %i.", main_button.read(),
+                battery_voltage_v, data.x, data.y, data.z);
 
-        tr_info("7");
-        backlight_high = true;
-        backlight_mid = true;
-        backlight_low = true;
         rtos::ThisThread::sleep_for(500);
-
-        // tr_info("6");
-        // backlight_high = true;
-        // backlight_mid = true;
-        // backlight_low = false;
-        // rtos::ThisThread::sleep_for(500);
-        // tr_info("5");
-        // backlight_high = true;
-        // backlight_mid = false;
-        // backlight_low = true;
-        // rtos::ThisThread::sleep_for(500);
-
-        // tr_info("4");
-        // backlight_high = true;
-        // backlight_mid = false;
-        // backlight_low = false;
-        // rtos::ThisThread::sleep_for(500);
-
-        // tr_info("3");
-        // backlight_high = false;
-        // backlight_mid = true;
-        // backlight_low = true;
-        // rtos::ThisThread::sleep_for(500);
-
-        // tr_info("2");
-        // backlight_high = false;
-        // backlight_mid = true;
-        // backlight_low = false;
-        // rtos::ThisThread::sleep_for(500);
-
-        // tr_info("1");
-        // backlight_high = false;
-        // backlight_mid = false;
-        // backlight_low = true;
-        // rtos::ThisThread::sleep_for(500);
-
-        // tr_info("0");
-        // backlight_high = false;
-        // backlight_mid = false;
-        // backlight_low = false;
-        // rtos::ThisThread::sleep_for(500);
-        
-
-
     }
+}
+
+void set_backlight(uint8_t light_level)
+{
+    std::bitset<3> bits(light_level);
+
+    backlight_high = bits[2];  
+    backlight_mid = bits[1];
+    backlight_low = bits[0];
 }
 
 void print_rslt(uint16_t rslt)
@@ -215,14 +157,14 @@ void print_rslt(uint16_t rslt)
     case BMA4_E_NULL_PTR:
         tr_err("Error [%d] : Null pointer", rslt);
         break;
-    // case BMA400_E_COM_FAIL:
-    //     tr_err("Error [%d] : Communication failure", rslt);
-    //     break;
-    // case BMA400_E_DEV_NOT_FOUND:
-    //     tr_err("Error [%d] : Device not found", rslt);
-    //     break;
+    case BMA4_E_OUT_OF_RANGE:
+        tr_err("Error [%d] : Out of range", rslt);
+        break;
     case BMA4_E_INVALID_SENSOR:
         tr_err("Error [%d] : Invalid sensor", rslt);
+        break;
+    case BMA4_E_CONFIG_STREAM_ERROR:
+        tr_err("Error [%d] : Config stream error", rslt);
         break;
     case BMA4_E_SELF_TEST_FAIL:
         tr_err("Warning [%d] : Self test failed", rslt);
@@ -233,8 +175,7 @@ void print_rslt(uint16_t rslt)
     }
 }
 
-uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
-                     uint16_t length)
+uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
 {
 
     /* Write to registers using I2C. Return 0 for a successful execution. */
@@ -248,7 +189,7 @@ uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
     tmp[0] = reg_addr;
     memcpy(tmp + 1, reg_data, length);
 
-    ret = i2c.write(i2c_addr<<1, (const char *)tmp, length + 1, false);
+    ret = i2c.write(i2c_addr << 1, (const char *)tmp, length + 1, false);
 
     if (!ret)
     {
@@ -258,22 +199,21 @@ uint16_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
     return -1;
 }
 
-uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
-                    uint16_t length)
+uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
 {
 
     /* Read from registers using I2C. Return 0 for a successful execution. */
 
     int ret = 0;
 
-    ret = i2c.write(i2c_addr<<1, (const char *)&reg_addr, 1, true);
+    ret = i2c.write(i2c_addr << 1, (const char *)&reg_addr, 1, true);
     if (!ret)
     {
-        ret = i2c.read(i2c_addr<<1, (char *)reg_data, length, false);
+        ret = i2c.read(i2c_addr << 1, (char *)reg_data, length, false);
     }
     else
     {
-    	tr_err("I2C Read Failed writting address, returned %i.", ret);
+        tr_err("I2C Read Failed writting address, returned %i.", ret);
     }
 
     if (!ret)
@@ -285,20 +225,3 @@ uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data,
 }
 
 void delay_ms(uint32_t period) { rtos::ThisThread::sleep_for(period); }
-
-// void set_interface(enum bma400_intf intf, struct bma400_dev *dev)
-// {
-//     switch (intf)
-//     {
-//     case BMA400_I2C_INTF:
-//         dev->intf_ptr = NULL; /* To attach your interface device reference */
-//         dev->delay_ms = delay_ms;
-//         dev->dev_id = BMA400_I2C_ADDRESS_SDO_HIGH;
-//         dev->read = i2c_reg_read;
-//         dev->write = i2c_reg_write;
-//         dev->intf = BMA400_I2C_INTF;
-//         break;
-//     default:
-//         tr_err("Interface not supported.");
-//     }
-// }
