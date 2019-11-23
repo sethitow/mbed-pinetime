38453e0fc5d062f713e351a48267c3368ceb2702
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-11-22 21:50:20 -0800

    Pixel painting app!

--- apps/periph_test/source/main.cpp
+++ apps/periph_test/source/main.cpp
@@ -11,9 +11,11 @@
 #include "rtos/ThisThread.h"
 #include "rtos/Thread.h"
 
+#include "Adafruit_ST7789.h"
 #include "BMA421_Accelerometer.h"
-#include "HRS3300_HeartRateSensor.h"
 #include "CST0xx_TouchPad.h"
+#include "HRS3300_HeartRateSensor.h"
+
 #include "mbed-rtt.h"
 
 #include <bitset>
@@ -30,6 +32,7 @@ FileHandle *mbed_override_console(int fd)
 }
 } // namespace mbed
 
+void on_touch_event(struct CST0xx_TouchPad::ts_event event);
 uint16_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 void set_backlight(uint8_t light_level);
 void task_500ms();
@@ -49,11 +52,14 @@ mbed::DigitalIn charge_indication(PIN_CHARGE_INDICATION_IN);
 mbed::InterruptIn touchpad_interrupt(PIN_TOUCHPAD_INTERRUPT);
 mbed::InterruptIn main_button_interrupt(PIN_PUSH_BUTTON_OUT);
 
-mbed::DigitalOut lcd_cs(SPI_CS_LCD);
+// mbed::DigitalOut lcd_cs(SPI_CS_LCD);
+mbed::DigitalOut spi_flash_cs(SPI_CS_FLASH);
 
 mbed::I2C i2c(I2C_SDA, I2C_SCL);
 HRS3300_HeartRateSensor hrs(&i2c);
-CST0xx_TouchPad touch_pad(&i2c);
+CST0xx_TouchPad touch_pad(&i2c, callback(on_touch_event));
+
+Adafruit_ST7789 display(SPI_MOSI, SPI_MISO, SPI_SCK, SPI_CS_LCD, PIN_LCD_RS_OUT, PIN_LCD_RESET_OUT);
 
 static rtos::Mutex trace_mutex;
 static void trace_mutex_wait() { trace_mutex.lock(); }
@@ -68,21 +74,19 @@ int main()
     mbed_trace_init();
 
     tr_info("Booting...");
-    lcd_cs = true;    // Disable LCD SPI communications
+    // lcd_cs = true;    // Disable LCD SPI communications
+    spi_flash_cs = true;    // Disable SPI Flash communications
     vibrator = true;  // Turns the vibrator off
-    set_backlight(7); // Turn the backlight off
+    set_backlight(1); // Turn the backlight off
 
-    mbed::BlockDevice *bd = mbed::BlockDevice::get_default_instance();
-    tr_info("BD init returned %i", bd->init());
-    tr_info("BD Size %lld.", bd->size());
+    // mbed::BlockDevice *bd = mbed::BlockDevice::get_default_instance();
+    // tr_info("BD init returned %i", bd->init());
+    // tr_info("BD Size %lld.", bd->size());
 
     i2c.frequency(100000);
 
     hrs.set_enable(HRS3300_HeartRateSensor::HRS_DISABLE,
                    HRS3300_HeartRateSensor::HRS_WAIT_TIME_400ms);
-
-
-
     // BMA421_Accelerometer accel(&i2c);
 
     // printf("I2C Scan...\r\n");
@@ -109,6 +113,34 @@ int main()
     main_button_interrupt.rise(event_queue.event(main_button_on_rise));
     main_button_interrupt.fall(event_queue.event(main_button_on_fall));
 
+
+    tr_info("display init");
+    display.init();
+    tr_info("fillScreen");
+    display.fillScreen(0x07E0);
+    
+    // uint16_t color = 0xF800;
+    // int t;
+    // int w = display.width() / 2;
+    // int x = display.height() - 1;
+    // int y = 0;
+    // int z = display.width();
+    // for (t = 0; t <= 15; t++)
+    // {
+    //     tr_info("drawTriangle");
+    //     display.drawTriangle(w, y, y, x, z, x, color);
+    //     x -= 4;
+    //     y += 4;
+    //     z -= 4;
+    //     color += 100;
+    // }
+
+    // display.setCursor(50,50);
+    // display.setTextColor(0xF800, 0xFFFF);
+    // display.printf("Hi");
+
+
+
 }
 
 void task_500ms()
@@ -124,7 +156,8 @@ void task_500ms()
     uint32_t hrs_val = hrs.read_heart_rate_sensor();
     uint32_t als_val = hrs.read_ambient_light_sensor();
 
-    tr_info("Batt Volt: %f. Accel Data: %i, %i, %i. HRS: %lu. ALS: %lu.", battery_voltage_v, data.x, data.y, data.z, hrs_val, als_val);
+    tr_info("Batt Volt: %f. Accel Data: %i, %i, %i. HRS: %lu. ALS: %lu.", battery_voltage_v, data.x,
+            data.y, data.z, hrs_val, als_val);
 }
 
 void set_backlight(uint8_t light_level)
@@ -136,16 +169,12 @@ void set_backlight(uint8_t light_level)
     backlight_low = bits[0];
 }
 
-void main_button_on_rise()
+void on_touch_event(struct CST0xx_TouchPad::ts_event event)
 {
-    tr_info("Main Button Rise");
+    tr_info("MAIN! X: %u Y: %u", event.au16_x[0], event.au16_y[0]);
+    display.fillRect(event.au16_x[0], event.au16_y[0], 10, 10, 0xF800);
 }
 
-void main_button_on_fall()
-{
-    tr_info("Main Button Fall");
-}
+void main_button_on_rise() { tr_info("Main Button Rise"); }
 
-
-
-
+void main_button_on_fall() { tr_info("Main Button Fall"); }
